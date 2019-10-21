5d9f43149a0a73bedce01ea611ba8496acc0cf58
Илья Гладких <gladkihiu@outlook.com>
2019-10-21 18:02:39 +0300

    21.10.2019

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("SvodExcel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Программа для сведения данных из Excel-файлов расписания для ИПК Тулы")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ИПК Тула")]
 [assembly: AssemblyProduct("SvodExcel")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Гладких И.Ю. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
