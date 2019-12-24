7fcf3c7797b95fcd38709e793e85aa64e2c93a69
Илья Гладких <gladkihiu@outlook.com>
2019-12-24 15:43:41 +0300

    update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1204.1")]
+[assembly: AssemblyVersion("1.7.1224.1")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: NeutralResourcesLanguage("ru")]
